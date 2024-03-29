e2e70c12f39e20612bc882d94bb3ced42880f697
Daniel Easterman <daniel.easterman@gmail.com>
2024-03-29 18:30:27 +0000

    Add automatic revision parameter

--- __appsignal__.py
+++ __appsignal__.py
@@ -1,11 +1,19 @@
 import os
+import subprocess
 from appsignal import Appsignal
 from dotenv import load_dotenv
 load_dotenv()
+
+revision = subprocess \
+    .run(["git", "log", "--pretty=format:%h", "-n 1"], stdout=subprocess.PIPE) \
+    .stdout \
+    .decode("utf-8")
 
 appsignal = Appsignal(
     active=True,
     name="fastapi-render-appsignal",    
     # https://docs.appsignal.com/python/configuration/options.html#option-push_api_key
-    push_api_key=os.getenv("APPSIGNAL_API_KEY")
+    push_api_key=os.getenv("APPSIGNAL_API_KEY"),
+	revision=revision
 )
+
