3d59583ef6092239df2674a9dc320baf4ba2cf62
Daniel Easterman <daniel.easterman@gmail.com>
2024-03-29 19:15:59 +0000

    Remove commented url

--- __appsignal__.py
+++ __appsignal__.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 revision = None
-
 try:
     revision = subprocess.check_output(
         "git log --pretty=format:'%h' -n 1", shell=True
@@ -13,10 +12,10 @@ try:
 except subprocess.CalledProcessError:
   pass
 
+
 appsignal = Appsignal(
     active=True,
     name="fastapi-render-appsignal",    
-    # https://docs.appsignal.com/python/configuration/options.html#option-push_api_key
     push_api_key=os.getenv("APPSIGNAL_API_KEY"),
 	revision=revision,
     environment="production"
